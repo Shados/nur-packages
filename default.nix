d73e342e1b387c4b3fd23f7e4712205fa354b310
Alexei Robyn <shados@shados.net>
2020-06-10 18:38:22 +1000

    pkgs: Add urxvt-config-reload

--- default.nix
+++ default.nix
@@ -3,4 +3,9 @@
   lib = import ./lib { inherit pkgs; };
   modules = import ./modules;
   overlays = import ./modules;
+
+  urxvt-config-reload = pkgs.callPackage ./pkgs/urxvt-config-reload {
+    inherit (pkgs.perlPackages) AnyEvent LinuxFD CommonSense SubExporter
+      DataOptList ParamsUtil SubInstall;
+  };
 }
