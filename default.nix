46463c7f96145b5e77f094a7c4b26595ae06ffcf
Alexei Robyn <shados@shados.net>
2020-06-10 20:31:11 +1000

    pkgs/waterfox: Init at 2020.06-current

--- default.nix
+++ default.nix
@@ -1,8 +1,29 @@
 { pkgs ? import <nixpkgs> { } }:
-{
+let
+  nixpkgsPath = pkgs.path;
+in
+rec {
   lib = import ./lib { inherit pkgs; };
   modules = import ./modules;
   overlays = import ./modules;
+
+  ## Packages
+
+  firefox-common = opts: with pkgs; callPackage
+    (import "${pkgs.path}/pkgs/applications/networking/browsers/firefox/common.nix" opts)
+    { inherit (gnome2) libIDL;
+      libpng = libpng_apng;
+      gnused = gnused_422;
+      icu = icu63;
+      inherit (darwin.apple_sdk.frameworks) CoreMedia ExceptionHandling
+                                            Kerberos AVFoundation MediaToolbox
+                                            CoreLocation Foundation AddressBook;
+      inherit (darwin) libobjc;
+      inherit (rustPackages) cargo rustc;
+
+      enableOfficialBranding = false;
+      privacySupport = true;
+    };
 
   tagsistant = pkgs.callPackage ./pkgs/tagsistant { };
   urxvt-config-reload = pkgs.callPackage ./pkgs/urxvt-config-reload {
@@ -12,5 +33,13 @@
   urxvtconfig = pkgs.callPackage ./pkgs/urxvtconfig {
     inherit (pkgs.qt5) qtbase qmake;
     inherit (pkgs.xorg) libXft;
+  };
+
+  waterfox = pkgs.wrapFirefox waterfox {
+    browserName = "waterfox";
+    nameSuffix = "";
+  };
+  waterfox-unwrapped = pkgs.callPackage ./pkgs/waterfox {
+    inherit firefox-common nixpkgsPath;
   };
 }
