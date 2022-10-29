a1944527068ee2be2a8c3baa46e5c187a863f7c6
Alexei Robyn <shados@shados.net>
2022-10-30 01:35:56 +1100

    overlays/lua-{overrides,packages}: Rework the override interface a little

--- overlays/lua-overrides.nix
+++ overlays/lua-overrides.nix
@@ -8,25 +8,22 @@ self: super: with super.lib; let
     (nameAndPkgs "luajit" "luajitPackages" )
   ];
   nameAndPkgs = name: pkgs: { interpreter = name; packages = pkgs; };
-  overrideLuaPackages = luaPackages: overrides: let
-    baseOverride = (luaself: luasuper: {});
-    composedOverride = foldl' (composeExtensions) (baseOverride) overrides;
-  in composedOverride;
+
+  overriddenLuas = concatMap overriddenLua luaNames;
   overriddenLua = {interpreter, packages}: let
-    packageOverrides = overrideLuaPackages super.${interpreter}.pkgs self.lib.luaOverrides;
+    packageOverrides = composeManyExtensions self.lib.luaOverrides;
     newLua = super.${interpreter}.override { inherit packageOverrides; };
   in [
     { name = interpreter; value = newLua; }
     { name = packages; value = newLua.pkgs; }
   ];
-  overriddenLuas = concatMap overriddenLua luaNames;
 in {
   lib = (super.lib or { }) // {
     # We use this by specifying overrides in the below, they get fed down into all luaPackages
     luaOverrides = [];
     defineLuaPackageOverrides = super: overrides: {
       lib = super.lib // {
-        luaOverrides = super.lib.luaOverrides ++ [ overrides ];
+        luaOverrides = super.lib.luaOverrides ++ overrides;
       };
     };
   };
