89bbc7989900ed08b8307ed3d09d92f3f6adb0ec
Alexei Robyn <shados@shados.net>
2023-08-17 22:32:14 +1000

    Attempt to work around issue in NUR eval

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 let
   nixpkgsPath = pkgs.path;
   pins = import ./nix/sources.nix nixpkgsPath pkgs.targetPlatform.system;
-  naersk = pkgs.callPackage (import pins.naersk.outPath) { };
+  naersk = pkgs.callPackage (import (pins.naersk.outPath + /default.nix)) { };
 in
 let repo = rec {
   lib = import ./lib { inherit pkgs repo; };
