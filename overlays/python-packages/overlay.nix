ced4e42714358bdb5bc1d5442b5740826bad9964
Alexei Robyn <shados@shados.net>
2024-04-04 14:28:40 +1100

    overlays/python-packages: Drop unneeded packages

--- overlays/python-packages/overlay.nix
+++ overlays/python-packages/overlay.nix
@@ -2,10 +2,6 @@ self: super: with super.lib; let
   pins = import ../../nix/sources.nix super.path super.targetPlatform.system;
 in {
   pythonOverrides = self.lib.buildPythonOverrides (pyself: pysuper: {
-    # General language-specific support tools
-    flake8-bugbear = pyself.callPackage ./flake8-bugbear.nix { };
-    flake8-per-file-ignores = pyself.callPackage ./flake8-per-file-ignores.nix { };
-
     usb-resetter = pyself.callPackage ./usb-resetter.nix { inherit pins; };
   }) super.pythonOverrides;
 }
